fee223269cacdc0da7b0163aa2ac9b1d50972fd3
Neha Thakur <nehat280@gmail.com>
2023-07-01 20:08:42 +0530

    test premium and mini

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -33,3 +33,51 @@ class TestCompactType:
     def test_return(self, customer_details):
         self.car_rental.rent(**customer_details)
         assert self.car_rental.return_car(10, "2023-8-01") == 310
+
+
+class TestPremiumType:
+
+    def setup_class(self):
+        self.car_rental = CarRental(car_category="Premium")
+
+    @staticmethod
+    @pytest.fixture
+    def customer_details():
+        return {"customer_name": "Test user",
+                "car_mileage_before": 5,
+                "date_rented": "2023-07-01"}
+
+    def test_create_car_rental(self):
+        assert self.car_rental.car_category == "Premium"
+
+    def test_category_true_after_rent(self, customer_details):
+        self.car_rental.rent(**customer_details)
+        assert self.car_rental.car_category_rented["Premium"]
+
+    def test_return(self, customer_details):
+        self.car_rental.rent(**customer_details)
+        assert self.car_rental.return_car(10, "2023-8-01") == 412.225
+
+
+class TestMinivanType:
+
+    def setup_class(self):
+        self.car_rental = CarRental(car_category="Minivan")
+
+    @staticmethod
+    @pytest.fixture
+    def customer_details():
+        return {"customer_name": "Test user",
+                "car_mileage_before": 5,
+                "date_rented": "2023-07-01"}
+
+    def test_create_car_rental(self):
+        assert self.car_rental.car_category == "Minivan"
+
+    def test_category_true_after_rent(self, customer_details):
+        self.car_rental.rent(**customer_details)
+        assert self.car_rental.car_category_rented["Minivan"]
+
+    def test_return(self, customer_details):
+        self.car_rental.rent(**customer_details)
+        assert self.car_rental.return_car(10, "2023-8-01") == 587.3375
